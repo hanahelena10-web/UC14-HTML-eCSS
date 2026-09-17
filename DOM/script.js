720304840e10fdc5bcd13c1c562ddb8b380771c6
hanahelena10-web <hanahelena10@gmail.com>
2026-09-17 17:42:27 -0300

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,8 @@
+function mudarTexto () {
+    document.getElementById("titulo").
+textContent = "voce clicou no botao";
+}
+    function mudarcor(){
+        document.getElementById("menssagem").
+        Style.color = "blue"
+    }
